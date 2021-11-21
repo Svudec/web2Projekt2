589dadf4f01b49bff94652c0c8aa6a67563a988f
Karlo Sudec <Svudec@users.noreply.github.com>
2021-11-21 21:11:31 +0100

    Get data from database

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const { Pool, Client } = require('pg');
+
+//Pool uses env for database connection settings
+const pool = new Pool()
+
 const port = 3000;
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' });
+
+    pool.query('SELECT * FROM grades WHERE owner_id=1', (err, resDb) => {
+        if (err) {
+          throw err;
+
+        } else {
+          console.log(resDb.rows)
+          let grades = resDb.rows;
+
+          res.render('index', { grades: grades });
+        }
+      });
 });
 
 app.listen(port, () => {
